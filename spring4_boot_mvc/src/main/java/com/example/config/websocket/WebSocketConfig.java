fd70512f2399628762582d018d6f50ef93f87535
sungang <1120_sungang@gmail.com>
2016-12-15 16:35:26 +0800

    增加websocket demo

---
+++ spring4_boot_mvc/src/main/java/com/example/config/websocket/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.example.config.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by sungang on 2016/12/15.
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
