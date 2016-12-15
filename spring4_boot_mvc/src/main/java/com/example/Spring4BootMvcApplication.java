c44b96fd1e9b188ee8b9a24e305d05bfd9ff215b
sungang <1120_sungang@gmail.com>
2016-12-15 16:42:13 +0800

    update

--- spring4_boot_mvc/src/main/java/com/example/Spring4BootMvcApplication.java
+++ spring4_boot_mvc/src/main/java/com/example/Spring4BootMvcApplication.java
@@ -2,9 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//
 @SpringBootApplication
-public class Spring4BootMvcApplication {
+public class Spring4BootMvcApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Spring4BootMvcApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Spring4BootMvcApplication.class);
